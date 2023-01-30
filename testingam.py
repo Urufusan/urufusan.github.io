fa19d2458c932a999643e2f4a109818e23e598e7
Urufusan <petarmesic8@gmail.com>
2023-01-30 19:25:03 +0100

    Fixed flask backend, added https

--- testingam.py
+++ testingam.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
+import flask
 import requests
 
-app = Flask(__name__)
-
+app = flask.Flask(__name__)
+context = ("../certs/cert.pem", "../certs/pkey.pem")
 @app.route("/jsonparser")
 def jsonparser():
     response = requests.get("http://192.168.23.17/")
     json_data = response.json()
-    return jsonify(json_data)
+    cliresp = flask.jsonify(json_data)
+    cliresp.headers.add('Access-Control-Allow-Origin', '*')
+    return cliresp
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True, host="0.0.0.0" , ssl_context=context) ##  ssl_context="adhoc")
+
